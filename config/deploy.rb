b21b7d789a1a3399e475e0ec1b1c65fd6230c314
Kim Buckley <kbuckley@nuw.org.au>
2016-03-11 12:05:06 +1100

    deploy to locally precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,9 @@ set :puma_init_active_record, true
 set :linked_files, %w{config/application.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+# Clear existing task so we can replace it rather than "add" to it.
+Rake::Task["deploy:compile_assets"].clear 
+
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
@@ -55,6 +58,38 @@ namespace :puma do
 end
 
 namespace :deploy do
+  # Customised for local precompile
+  desc 'Compile assets'
+  task :compile_assets => [:set_rails_env] do
+    # invoke 'deploy:assets:precompile'
+    invoke 'deploy:assets:precompile_local'
+    invoke 'deploy:assets:backup_manifest'
+  end
+
+
+  namespace :assets do
+    desc "Precompile assets locally and then rsync to web servers" 
+    task :precompile_local do 
+      # compile assets locally
+      run_locally do
+        execute "RAILS_ENV=#{fetch(:stage)} bundle exec rake assets:precompile"
+      end
+
+      # rsync to each server
+      local_dir = "./public/assets/"
+      on roles( fetch(:assets_roles, [:web]) ) do
+        # this needs to be done outside run_locally in order for host to exist
+        remote_dir = "#{host.user}@#{host.hostname}:#{release_path}/public/assets/"
+    
+        run_locally { execute "rsync -av --delete #{local_dir} #{remote_dir}" }
+      end
+
+      # clean up
+      # run_locally { execute "rm -rf #{local_dir}" }
+      run_locally { execute :bundle, "exec rake assets:clobber" }
+    end
+  end
+  
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
