3cf83b62cc89b1d3c3dd7da3a11317867c309583
Kim Buckley <kbuckley@nuw.org.au>
2016-03-17 18:20:54 +1100

    restored time tags to documents, fixed zh-TW typo

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,6 +2,10 @@ module DocumentsHelper
   def date_from_params(field)
     params[field] && Date.parse(params[field]) || Date.today
   end
+  
+  def date_to_time_tag(date)
+    "<time datetime=#{date}>#{date.to_s(:long)}</time>"
+  end
 
   def basename(doc)
     if doc.evidence.present?
