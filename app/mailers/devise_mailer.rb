91757797981bf9042a5809c21f01083e820f5711
Kim Buckley <kbuckley@nuw.org.au>
2016-05-01 14:30:40 +1000

    Added fallback inviter for invitation mail

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,10 +2,10 @@ class DeviseMailer < Devise::Mailer
   helper :application
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
-  
+
   def invitation_instructions(record, token, opts={})
-    @inviter = record.invited_by
-    
+    @inviter = (record.invited_by || User.first)
+
     opts[:subject] = I18n.t("devise.mailer.invitation_instructions.subject")
     opts[:from] = "#{@inviter.full_name} <#{@inviter.email}>"
     opts[:reply_to] = opts[:from]
