4c5a9bd199feb027eb3ea9c81499c9bb673f8fe6
Kim Buckley <kbuckley@nuw.org.au>
2016-06-20 12:43:21 +1000

    seed first four awards and NES

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,8 @@ grp = Group.create(
     "nav_bg_color" => "#7B001F",
     "nav_text_color" => "#DDD"
   },
-  supergroup: sg
+  supergroup: sg,
+  awards: { "nes" => "I'm not sure", "horticulture" => "Horticulture, e.g. fruit picking", "meat" => "Meat industry", "poultry" => "Poultry processing", "storage" => "Packing, storage etc." }
 )
 
 user = User.new(
@@ -54,8 +55,48 @@ profile = Profile.create(
   address: home_address
 )
 
-claim = Claim.create(
-  award: "horticulture",
+nes = Award.create(
+name: "National Employment Standards",
+short_name: "nes",
+default_minimum: BigDecimal.new(21.61, 10),
+min_casual_rates: { "2016" => 22.13, "2015" => 21.61, "2014" => 21.09, "2013" => 20.46, "2012" => 19.95, "2011" => 19.39, "2010" => 18.75 },
+min_permanent_rates: { "2016" => 17.70, "2015" => 17.29, "2014" => 16.87, "2013" => 16.37, "2012" => 15.96, "2011" => 15.51, "2010" => 15.00 }
+)
+
+horticulture = Award.create(
+name: "Horticulture Award 2010",
+short_name: "horticulture",
+default_minimum: BigDecimal.new(21.61, 10),
+min_casual_rates: { "2016" => 22.13, "2015" => 21.61, "2014" => 21.09, "2013" => 20.46, "2012" => 19.95, "2011" => 19.39, "2010" => 18.75 },
+min_permanent_rates: { "2016" => 17.70, "2015" => 17.29, "2014" => 16.87, "2013" => 16.37, "2012" => 15.96, "2011" => 15.51, "2010" => 15.00 }
+)
+
+meat = Award.create(
+name: "Meat Industry Award 2010",
+short_name: "meat",
+default_minimum: BigDecimal.new(21.61, 10),
+min_casual_rates: { "2016" => 22.13, "2015" => 21.61, "2014" => 21.09, "2013" => 20.46, "2012" => 19.95, "2011" => 19.39, "2010" => 18.75 },
+min_permanent_rates: { "2016" => 17.70, "2015" => 17.29, "2014" => 16.87, "2013" => 16.37, "2012" => 15.96, "2011" => 15.51, "2010" => 15.00 }
+)
+
+poultry = Award.create(
+name: "Poultry Processing Award 2010",
+short_name: "poultry",
+default_minimum: BigDecimal.new(22.34, 10),
+min_casual_rates: { "2016" => 22.87, "2015" => 22.34, "2014" => 21.79, "2013" => 21.15, "2012" => 20.61, "2011" => 20.04, "2010" => 19.38 },
+min_permanent_rates: { "2016" => 18.30, "2015" => 17.87, "2014" => 17.43, "2013" => 16.92, "2012" => 16.49, "2011" => 16.03, "2010" => 15.50 }
+)
+
+storage = Award.create(
+name: "Storage Services Award 2010",
+short_name: "storage",
+default_minimum: BigDecimal.new(23.09, 10),
+min_casual_rates: { "2016" => 23.64, "2015" => 23.09, "2014" => 22.52, "2013" => 21.87, "2012" => 21.32, "2011" => 20.71, "2010" => 20.03 },
+min_permanent_rates: { "2016" => 18.91, "2015" => 18.47, "2014" => 18.02, "2013" => 17.49, "2012" => 17.05, "2011" => 16.57, "2010" => 16.03 }
+)
+
+claim = Claim.new(
+  award: horticulture,
   weekly_hours: 35, 
   hourly_pay: 11.23, 
   employment_type: "casual", 
