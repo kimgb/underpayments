b1e7928b7576bcd0dd0482190b8c1f31e8f0470e
Kim Buckley <kbuckley@nuw.org.au>
2016-06-22 21:25:09 +1000

    fix i18n mixup, letter addressing

--- app/controllers/admin/letters_controller.rb
+++ app/controllers/admin/letters_controller.rb
@@ -9,7 +9,7 @@ class Admin::LettersController < Admin::BaseController
   # POST /admin/users/1/letters
   def create
     @letter = Letter.new(letter_params)
-    @addressee = Company.find_by_name(@letter.addressee)
+    @address = (Company.find_by_name(@letter.addressee) || Company.find_by_contact(@letter.addressee))
     
     render :show, layout: false
   end
