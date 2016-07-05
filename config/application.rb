7deca3365aaeb1e829f3a2d081e8ab60b2cfa96c
Kim Buckley <kbuckley@nuw.org.au>
2016-07-05 12:54:47 +1000

    fix password reset layout, add Korean available locale

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Underpaid
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :"en-AU"
-    config.i18n.available_locales = [:en, :"en-AU", :zh, :"zh-TW", :vi]
+    config.i18n.available_locales = [:en, :"en-AU", :zh, :"zh-TW", :vi, :ko]
     config.i18n.fallbacks = {
       :"en-AU" => :en,
       :"zh-TW" => :zh,
