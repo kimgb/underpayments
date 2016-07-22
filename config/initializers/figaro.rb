b4a9884cb2e35c90dcfd077f3fa956f6246bf565
Kim Buckley <kbuckley@nuw.org.au>
2016-07-22 13:04:49 +1000

    improved readability for summary of calc

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,5 @@
-Figaro.require_keys("db_user", "db_pass", "mailgun_api_key", "mailgun_domain", "app_host", "app_port", "secret_key_base")
+Figaro.require_keys(
+  "db_user", "db_pass", 
+  "mailgun_api_key", "mailgun_domain", 
+  "app_host", "app_port", "secret_key_base"
+)
