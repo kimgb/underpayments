eec14d06cdb2444ac50d7dab47eba944c9193890
Kim Buckley <kbuckley@nuw.org.au>
2016-08-22 16:00:23 +1000

    bugfix for 2009-2010 FY claim estimates

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,15 +1,15 @@
 class Award < ActiveRecord::Base
   extend FriendlyId
   friendly_id :short_name, use: :slugged
-  
+
   has_many :claims
-  
+
   validates_presence_of :name, :short_name, :min_casual_rates, :min_permanent_rates
   validates_uniqueness_of :name, :short_name
-  
+
   def minimum(emp_type, year)
-    year = year.to_s
-    
+    year = [year, 2010].max.to_s
+
     if emp_type == "permanent"
       BigDecimal(min_permanent_rates.dig(year))
     else
--- test/models/claim_test.rb
+++ test/models/claim_test.rb
@@ -1,88 +1,90 @@
 require_relative '../test_helper'
 
-class ClaimTest < ActiveSupport::TestCase  
+class ClaimTest < ActiveSupport::TestCase
   test "Claim::presentable_attributes" do
     assert_includes Claim.presentable_attributes, "lost_wages"
     refute_includes Claim.presentable_attributes, "id"
   end
-  
+
   test "#display_affidavit?" do
     # turn on when fixtures complete.
     # assert claims(:underpaid).display_affidavit?
     refute claims(:proper).display_affidavit?
   end
-  
+
   test "#enable_affidavit?" do
     # depends on full suite of associated fixtures for at least one claim
     skip "insufficient fixture coverage"
   end
-  
-  # three test cases: 
+
+  # three test cases:
   #   complete wage evidence;
   #   complete time evidence w/o complete wage evidence;
   #   incomplete wage and time evidence.
   test "#actual_pay" do
     skip "insufficient fixture coverage"
   end
-  
+
   test "ownership" do
     claim = claims(:underpaid_w_multi_docs)
-    
+
     assert_equal claim.owner, claim.user
     assert_equal nil, Claim.new.owner
     assert_includes claim.owners, claim.user
     assert_empty Claim.new.owners
   end
-  
+
   test "#stolen_wages" do
     assert claims(:underpaid).stolen_wages > 0
     assert claims(:underpaid_w_multi_docs).stolen_wages > 0
     assert claims(:proper).stolen_wages.zero?
   end
-  
+
   test "wages from evidence should only consider wage documents" do
     wfe = claims(:underpaid_w_multi_docs).send(:wages_from_evidence)
-    
+
     assert_equal "3000.0", wfe.to_s
   end
-  
+
   test "employment duration" do
     b_under = claims(:underpaid)
     employment_days_int = b_under.days.size
-    
+
     assert_equal b_under.send(:weeks_worked), employment_days_int / 7.0
     assert_instance_of Set, b_under.days
   end
-  
+
   test "award minimums" do
     assert_equal 21.61, claims(:underpaid).award_minimum #horticulture, NES casual
     assert_equal 22.34, claims(:poultry_underpaid).award_minimum #poultry casual
+
+    # TODO test a claim beginning 1 Jan 2010
   end
-  
+
   test "#document_coverage should be a set" do
   end
-  
+
   test "coverage gaps" do
     under, proper = claims(:underpaid), claims(:proper)
     employment_days_arr = [Array(under.employment_began_on..under.employment_ended_on)]
-    
+
     assert_instance_of Set, under.document_coverage
     assert_equal employment_days_arr, proper.coverage_gaps
     refute proper.coverage_complete?
     assert_empty under.coverage_gaps
     assert under.coverage_complete?
   end
-  
+
   test "coverage overlaps" do
     skip "not implemented yet"
   end
-  
+
   test "time estimation" do
     b_under = claims(:underpaid)
     ebo = b_under.employment_began_on
     year_1_weeks = b_under.send(:weeks_worked, ebo, ebo.end_of_fy)
     year_1_hours = year_1_weeks * b_under.weekly_hours
-    
+
     assert_equal year_1_hours, b_under.send(:estimated_hours_worked_by_year).values[0]
   end
 end
